9e07027cbfedc93b30dca16ac8e6631fc42e55bd
Shiiba <kshiiba@gmail.com>
2016-07-15 10:07:32 -0400

    starting models

--- controllers/ext_auth.js
+++ controllers/ext_auth.js
@@ -9,22 +9,22 @@ function deepPrint(x){
   console.log(util.inspect(x, {showHidden: false, depth: null}));
 }
 
-// router.post('/pocket_auth', (req,res) => {
-//   request(pocketUrl + , (error, response, body) => {
-//     if(!error && response.statusCode == 200){
-//       results = JSON.parse(body);
+router.post('/pocket_auth', (req,res) => {
+  request(pocketUrl + , (error, response, body) => {
+    if(!error && response.statusCode == 200){
+      results = JSON.parse(body);
       
 
-//       // var returnedSongs = [];
-//       // for(var i = 0; i < results.items.length; i++){
-//       //   var song = new Songs({
-//       //     videoId: results.items[i].id.videoId,
-//       //     title: results.items[i].snippet.title,
-//       //     img: results.items[i].snippet.thumbnails.default.url
-//       //   });
-//       //   returnedSongs.push(song);
-//     }
-// });
+      // var returnedSongs = [];
+      // for(var i = 0; i < results.items.length; i++){
+      //   var song = new Songs({
+      //     videoId: results.items[i].id.videoId,
+      //     title: results.items[i].snippet.title,
+      //     img: results.items[i].snippet.thumbnails.default.url
+      //   });
+      //   returnedSongs.push(song);
+    }
+});
 
 
 module.exports = router;
